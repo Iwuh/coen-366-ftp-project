91768817d67463a3cc6f77355b0588896df93cff
Matthew Faigan <matthew.faigan@gmail.com>
2022-12-09 15:21:27 -0500

    Finish untested server and response code

--- response.py
+++ response.py
@@ -22,3 +22,45 @@ def decode_first_byte(b: bytes) -> Tuple[ResponseType, int]:
     o = ResponseType(i >> 5)
     l = i & 0b00011111
     return (o, l)
+
+def encode_put_change_successful() -> bytes:
+    '''
+    Returns a bytes object representing a successful put or change operation.
+    '''
+    return (ResponseType.PUT_CHANGE.value << 5).to_bytes(1, 'big')
+
+def encode_get_successful(fileName: str, fileSize: int) -> bytes:
+    '''
+    Returns a bytes object representing a successful get operation.
+    fileName is the name of the file that was requested by the user.
+    fileSize is the size of the file that will be sent to the user.
+    Once this message is sent, it should be immediately followed by the file bytes.
+    '''
+    b1 = (ResponseType.GET.value << 5) + len(fileName)
+    return b1.to_bytes(1, 'big') + fileName.encode() + fileSize.to_bytes(4, 'big')
+
+def encode_get_not_found() -> bytes:
+    '''
+    Returns a bytes object representing a failed get operation.
+    '''
+    return (ResponseType.ERR_NOT_FOUND.value << 5).to_bytes(1, 'big')
+
+def encode_change_failed() -> bytes:
+    '''
+    Returns a bytes object representing a failed change operation.
+    '''
+    return (ResponseType.ERR_CHANGE_FAILED.value << 5).to_bytes(1, 'big')
+
+def encode_help_response() -> bytes:
+    '''
+    Returns a bytes object representing a help message.
+    '''
+    helpBytes = ('Cmds: put get change help bye').encode()
+    b1 = (ResponseType.HELP.value << 5) + len(helpBytes)
+    return b1.to_bytes(1, 'big') + helpBytes
+
+def encode_unknown_request_response() -> bytes:
+    '''
+    Returns a bytes object representing an unknown command opcode.
+    '''
+    return (ResponseType.ERR_UNKNOWN_REQUEST.value << 5).to_bytes(1, 'big')
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from socket import *
+import os
 
 import request
 import response
@@ -13,6 +14,48 @@ def main():
         while True:
             b1 = connectionSocket.recv(1)
             opcode, length = request.decode_first_byte(b1)
+            if opcode == request.RequestType.PUT:
+                handle_put(connectionSocket, length)
+            elif opcode == request.RequestType.GET:
+                handle_get(connectionSocket, length)
+            elif opcode == request.RequestType.CHANGE:
+                handle_change(connectionSocket, length)
+            elif opcode == request.RequestType.HELP:
+                handle_help(connectionSocket)
+            else:
+                connectionSocket.send(response.encode_unknown_request_response())
+
+def handle_put(s: socket, filenameLength: int):
+    fileName = s.recv(filenameLength).decode()
+    fileSize = int.from_bytes(s.recv(4), 'big')
+    fileData = s.recv(fileSize)
+    with open(fileName, "wb") as f:
+        f.write(fileData)
+    s.send(response.encode_put_change_successful())
+
+def handle_get(s: socket, filenameLength: int):
+    fileName = s.recv(filenameLength).decode()
+    try:
+        with open(fileName, "rb") as f:
+            content = f.read()
+            contentLength = len(content)
+            s.send(response.encode_get_successful(fileName, contentLength))
+            s.send(content)
+    except FileNotFoundError:
+        s.send(response.encode_get_not_found())
+
+def handle_change(s: socket, oldFilenameLength: int):
+    oldFilename = s.recv(oldFilenameLength).decode()
+    newFilenameLength = int.from_bytes(s.recv(1), 'big')
+    newFilename = s.recv(newFilenameLength).decode()
+    try:
+        os.rename(oldFilename, newFilename)
+        s.send(response.encode_put_change_successful())
+    except OSError:
+        s.send(response.encode_change_failed())
+
+def handle_help(s: socket):
+    s.send(response.encode_help_response())
 
 if __name__ == "__main__":
     main()
